0b5b36892115a65c48b9597658c420dc76e3f1e8
dilshodbekkarimovv <129936294+dilshodbekkarimovv@users.noreply.github.com>
2023-04-05 12:49:14 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,59 @@
+from baza_book import Database
+
+db = Database()
+
+# db.create_db()
+# db.create_category()
+# print('baza create')
+
+
+# db.insert_category('IT ADABIYOTLAR')
+# db.insert_category('ILMIY FANTASTIK ASARLAR')
+# db.insert_category('DINIY ADABIYOTLAR')
+# db.insert_category('KILASSIK ADABIYOTLAR')
+# print('category add')
+
+
+# category_id = db.select_category_id(1)
+# print(category_id)
+
+# data = db.select_category_all()
+# for i in data:
+# 	print(i)
+
+# # #SUB_CATEGORY
+
+# db.create_table_products()
+# print('create baza products')
+
+
+# db.insert_products("O'kinma","Asarga tarif: Ushbu kitob sizga:'Xushchaqchaq va baxtli bo'ling, yaxshilik tomonini izlang va tinchlaning' deydi. Hayotga o'z holicha boqishni, mamnun, quvnoq va shod yashashni o'rgatadi. yo'nalishlar, odamlar, zamon va makonlar bilan muomala qilishda fitratga zid xatolarni tuzatadi","3","30 000")
+# db.insert_products("O'nta megir bolasi","Asarga tarif: Agata Kristi nomini alohida tanishtirishning hojati yo'q. Uning mazkur asarida ham kitobxon sirli vaqiziqarli olamga sayohat qilishga muqarrar. Muallifning o'zi ham bu asarini bejiz eng yaxshi deb e'tirof etmaydi. Ushbu asarda bir-biriga hech qanday aloqasi bo'lmagan odamlarning oroldagi hayotidan hikoya qiladi","4","35 000")
+# db.insert_products("Qo'rqma","Asarga tarif: Roman o'tgan asrning yigirmanchi yillarida Germaniyada tahsil olgan va sobiq Sovet Ittifoqi tomonidan shafqatsizlarcha qatl etilgan millat yigit-qizlarnig xotirasiga bag'ishlanadi. O'tgan asr boshida Germaniyada o'qigan talabalar, ular maslagi va fojiali qismati bugungi yoshlar maqsadi bilan bir taroziga tortiladi.","2","35 000")
+# db.insert_products("Atom odatlar","Asarga tarif: Nima maqsad qo'yganigizdan qat'iy nazar, ushbu kitob sizga har kuni oz-ozdan yaxshilanib borishning sinalgan dasturini taklif qiladi. odatlarni shakillantirish bo'yicha dunyoning yetakchi ekspertlaridan biri Jeyms Klir yaxshi odatlarni shakillantirish, yomonlaridan xalos bo'lish hamda katta natijalarga olib boradi.","2","20 000")
+# db.insert_products("To'rtinchi sanoat inqilobi","Asarga tarif: 'To'rtinchi sanoat inqilobi' hodisasi o'zining o'lchami, ko'lami vamurakkabligi bilan insoniyat boshidan kechirgan hech bir jarayonga o'xshamaydi. Jismoniy, raqamli va biologik dunyoni birlashtirgan bir qator yangi taxnologiyalar bilan tavsiflangan ishlanmalar haqida ketadi","1","20 000")
+# db.insert_products("Halqa","Asarga tarif: Halqa qissasidagi voqealar oxirgi yuz yillikda bo'lib o'tadi. Qahramonlar qismati sizni goh Malaziya, goh Hind ummoni, goh Hind o'lkasi, Afg'on sahrolari, qadim Turkiston zamini,Bolqon yarimoroli va Uyg'ur diyorlariga yetaklaydi. Qahramonlar boshdan kechgan fojialar va ularning sabablari hech qachon eskirmaydi.","4","25 000")
+
+# print('products add')
+
+
+# data = db.select_products_for_category_id(1)
+# print(data)
+
+
+# data = db.select_product_id(6)
+
+# print(data)
+# dataall = db.select_product_all()
+
+# for i in dataall:
+# 	print(i)
+
+# data = db.search_product('ma')
+
+# print(data)
+
+
+
+
+
